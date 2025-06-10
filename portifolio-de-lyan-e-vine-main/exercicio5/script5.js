3fb572e36e0c2201934638d579eb5b518e031369
lyanxx2 <lyansantini@gmail.com>
2025-06-10 08:51:16 -0300

    Add files via upload

---
+++ portifolio-de-lyan-e-vine-main/exercicio5/script5.js
@@ -0,0 +1,10 @@
+function valor(){
+   let valor1= Number(window.prompt("digite o primeiro numero aqui:"));
+   let valor2= Number(window.prompt("digite o segundo numero "));
+   
+   if(valor1 > valor2){
+    alert("o numero:" + valor1 + " e o maior ")
+  }else{
+    alert("o numero: " + valor2 + "o maior ")
+  }
+    }
