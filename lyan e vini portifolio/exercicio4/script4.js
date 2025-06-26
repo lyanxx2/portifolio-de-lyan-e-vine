65ccb5db8e611cc135fcb7eebcd615bf9256ff0f
lyanxx2 <lyansantini@gmail.com>
2025-06-26 09:00:17 -0300

    Delete lyan e vini portifolio directory

--- lyan e vini portifolio/exercicio4/script4.js
+++
@@ -1,9 +0,0 @@
-let contador = 0
-let equipes= 0
-function inscrever(){
-
-contador += 1
-
-equipes = contador / 3
-alert("Possui " + contador + " no campeonato." + " Possui " + equipes + " equipes no campeonato")
-}
