c6c7f1971b21e70b1eddc9d4bff10ffd04623cd1
lyanxx2 <lyansantini@gmail.com>
2025-06-26 10:54:40 -0300

    Add files via upload

---
+++ exercicio5/script5.js
@@ -0,0 +1,10 @@
+function valor(){
+   let valor1= Number(window.prompt("digite o primeiro numero "));
+   let valor2= Number(window.prompt("digite o segundo numero "));
+   
+   if(valor1 > valor2){
+    alert("o número " + valor1 + " é o maior ")
+  }else{
+    alert("o número " + valor2 + " o maior ")
+  }
+}
