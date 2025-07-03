abc774dc2366d586372e031427c0e25121eb399b
lyanxx2 <lyansantini@gmail.com>
2025-07-03 10:51:11 -0300

    Add files via upload

---
+++ prova 2° trimentre lyan e vini/script.js
@@ -0,0 +1,29 @@
+
+function inscrever(){
+
+alert ("Digite o nome da equipe aqui:")
+  
+  let equipe = (window.prompt("aqui:"))
+  let jogador = (window.prompt("Digite o nome do primeiro jogador:"))
+  let jogador2 = (window.prompt("Digite o nome do segundo jogador:"))
+  let jogador3 = (window.prompt("Digite o nome do terceiro jogador:"))
+
+     
+  let idade1 = (window.prompt("digite a idade do primeiro jogador:"))
+  let idade2 = (window.prompt("digite a idade do segundo jogador:")) 
+  let idade3 = (window.prompt("digite a idade do terceiro jogador:"))
+ 
+  mostraHTML.innerHTML = equipe ;
+  mostrarHTML.innerHTML = jogador + idade1;
+  mostraraHTML.innerHTML = jogador2 + idade2;
+  mostrHTML.innerHTML = jogador3 + idade3;
+
+
+
+
+
+}
+
+
+
+
